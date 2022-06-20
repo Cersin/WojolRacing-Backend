866578b868405ec7d49b450b6c1e664531afe453
Szymon Wójcik <wojcik.szymon1@gmail.com>
2022-06-20 19:08:42 +0200

    feat(errors): handling

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,3 +1,29 @@
+import AppError from "~/server/utils/appError";
+
+const handleCastErrorDB = err => {
+    const message = `Błędny ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Taka nazwa już istnieje: ${value}. Proszę użyj innej.`
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+    // const errors = Object.values(err.errors).map(el => el.message); // ewentualnie tak
+    const errors = Object.values(err.errors).map(el => {
+        return {
+            message: el.message,
+            name: el.path
+        }
+    });
+
+    // const message = `${errors.join('. ')}`; // ewentualnie tak
+    return new AppError(JSON.stringify(errors), 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -16,7 +42,6 @@ const sendErrorProd = (err, res) => {
         });
         // Programming or other unknown error: dont want to leak details to the client
     } else {
-        console.error('Error: ', err);
         res.status(500).json({
             status: 'error',
             message: 'Something went wrong'
@@ -24,15 +49,27 @@ const sendErrorProd = (err, res) => {
     }
 }
 
-module.exports =  (err, req, res, next) => {
+export const globalErrorHandler =  (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
+        // sendErrorDev(err, res);
+        let error = Object.create(err);
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        sendErrorProd(error, res);
+
     }
 
     if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = Object.create(err);
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        sendErrorProd(error, res);
     }
 };
--- server/index.js
+++ server/index.js
@@ -6,9 +6,15 @@ import xss from 'xss-clean';
 import hpp from 'hpp';
 import cookieParser from 'cookie-parser';
 import AppError from "~/server/utils/appError";
-import globalErrorHandler from "~/server/controllers/errorController";
+import {globalErrorHandler} from "~/server/controllers/errorController";
 import { config } from "~/server/config-server";
 import userRoutes from "~/server/routes/userRoutes";
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION. SHUTTING DOWN');
+    process.exit(1);
+})
 
 const app = express();
 
@@ -30,6 +36,12 @@ app.use('/users', userRoutes);
 app.all('*', (req,res,next) => {
     next(new AppError(`Nie mogę znaleźć ${req.originalUrl} na tym serwerze!`, 404));
 });
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION. SHUTTING DOWN');
+    process.exit(1);
+})
 
 app.use(globalErrorHandler);
 
