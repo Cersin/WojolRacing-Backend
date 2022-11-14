9a3eb3bc5d6fe6162bc411d2c0e828a9706a0d76
Szymon Wójcik <wojcik.szymon1@gmail.com>
2022-11-14 08:52:58 +0100

    refactor: JWT

--- server/config-server.js
+++ server/config-server.js
@@ -1,5 +1,5 @@
 export const config = {
-    JWT_SECRET: 'ultra-secret-wojolracing-f1',
+    JWT_SECRET: 'ultra-secret-wojolracing-f1passw',
     JWT_EXPIRES_IN: '90d',
     JWT_COOKIE_EXPIRES_IN: '90',
     DATABASE_GLOBAL: 'mongodb://mo1714_wojolbaza:BBq1oV0BlqF2M3(WJtzo@mongo0.small.pl:27017/mo1714_wojolbaza',
